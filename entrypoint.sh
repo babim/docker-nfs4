5367ccd039cbb552c8ed1d5f939ea5b2e48ae8ea
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-28 12:45:35 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,11 +19,14 @@ FOLDER4IP=${FOLDER4IP:-*}
 
 # set config
 if [ ! -f "/nfs/exportfs" ]; then
+  touch /nfs/exportfs
+  
+if [[ ! -z "${FOLDER1}" ]]; then
 if [ ! -D "/nfs/$FOLDER1" ]; then mkdir -p /nfs/$FOLDER1; fi
 cat <<EOF>> /nfs/exportfs
 /nfs/$FOLDER1 $FOLDER1IP($FOLDER1SHARE)
 EOF
-
+fi
 if [[ ! -z "${FOLDER2}" ]]; then
 if [ ! -D "/nfs/$FOLDER2" ]; then mkdir -p /nfs/$FOLDER2; fi
 cat <<EOF>> /nfs/exportfs
