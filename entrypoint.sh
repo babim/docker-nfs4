6cfd37d36cecadf9c0b5eee894d005cd141f13c9
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-28 12:00:45 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,47 @@
 #!/bin/sh -eu
 
+# set value
+FOLDER1=${FOLDER1:-data}
+FOLDER1SHARE=${FOLDER1SHARE:-rw,fsid=0,root_squash,no_subtree_check,insecure}
+FOLDER1IP=${FOLDER1IP:-*}
+
+FOLDER2=${FOLDER2:-data}
+FOLDER2SHARE=${FOLDER2SHARE:-rw,fsid=0,root_squash,no_subtree_check,insecure}
+FOLDER2IP=${FOLDER2IP:-*}
+
+FOLDER3=${FOLDER3:-data}
+FOLDER3SHARE=${FOLDER3SHARE:-rw,fsid=0,root_squash,no_subtree_check,insecure}
+FOLDER3IP=${FOLDER3IP:-*}
+
+FOLDER4=${FOLDER4:-data}
+FOLDER4SHARE=${FOLDER4SHARE:-rw,fsid=0,root_squash,no_subtree_check,insecure}
+FOLDER4IP=${FOLDER4IP:-*}
+
+# set config
+if [ ! -f "/nfs/exportfs" ]; then
+cat <<EOF>> /nfs/exportfs
+/nfs/$FOLDER1 $FOLDER1IP($FOLDER1SHARE)
+EOF
+
+if [[ ! -z "${FOLDER2}" ]]; then
+cat <<EOF>> /nfs/exportfs
+/nfs/$FOLDER2 $FOLDER2IP($FOLDER2SHARE)
+EOF
+fi
+if [[ ! -z "${FOLDER3}" ]]; then
+cat <<EOF>> /nfs/exportfs
+/nfs/$FOLDER3 $FOLDER3IP($FOLDER3SHARE)
+EOF
+fi
+if [[ ! -z "${FOLDER4}" ]]; then
+cat <<EOF>> /nfs/exportfs
+/nfs/$FOLDER4 $FOLDER4IP($FOLDER4SHARE)
+EOF
+fi
+
+fi
+
+# run services
 /usr/sbin/exportfs -r
 /sbin/rpcbind --
 /usr/sbin/rpc.nfsd |:
