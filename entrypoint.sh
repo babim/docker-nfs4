209e399ede864ebce5b98ef0c2aae80a8aa3106d
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-04-16 17:21:15 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#! /bin/sh -e
+
 set -m
+
+mkdir -p "$MOUNTPOINT"
+
 rpcbind -f &
-mount "$@"
+mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
+mount | grep nfs
 fg
